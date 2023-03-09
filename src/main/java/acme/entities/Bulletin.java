22d19eead164ea59fd86e2fb59e534bc9ddddf4f
alvnavriv <alvnavriv@alum.us.es>
2023-03-09 11:23:59 +0100

    Añadida Relación con administrator

--- src/main/java/acme/entities/Bulletin.java
+++ src/main/java/acme/entities/Bulletin.java
@@ -4,12 +4,15 @@ package acme.entities;
 import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 
+import acme.framework.components.accounts.Administrator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,23 +22,23 @@ import lombok.Setter;
 public class Bulletin {
 
 	@Past
-	private Date	instationMoment;
+	private Date			instationMoment;
 
 	@NotBlank
 	@Length(max = 75)
-	private String	title;
+	private String			title;
 
 	@NotBlank
 	@Length(max = 100)
-	private String	message;
+	private String			message;
 
 	@NotNull
-	private Boolean	isCritical;
+	private Boolean			isCritical;
 
-	private String	link;
+	private String			link;
 
-	//	@ManyToOne
-	//    @JoinColumn(name = "admin_id")
-	//    private Admin admin;
+	@ManyToOne
+	@JoinColumn(name = "administrator_id")
+	private Administrator	administrator;
 
 }
